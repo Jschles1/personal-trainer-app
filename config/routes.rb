3c3b65edd18730262f60df2c647974b7f7b0416c
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 01:59:00 +0000

    Added routes for homepage, users, clients, sessions, login/logout, & omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+
+  resources :users
+  resources :clients
+  resources :sessions, only: [:create]
+
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/auth/facebook/callback', to: 'sessions#create'
 end
