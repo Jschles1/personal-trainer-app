f49aad3d10060367b0d2b1e3c38fb80fa4f23acf
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:29:38 +0000

    Added auth & normal_login methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,20 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+  def normal_login
+    @user = User.find_by(email: params[:user][:email])
+    if !!@user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to clients_path
+    else
+      # redirect to login form with errors
+    end
+  end
+
 end
