6fa530a9a55511aa5fcd22c8fcb37b8c2dc7cd37
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:58:02 +0000

    Added appointment model validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :client
   belongs_to :gym
+
+  validates_datetime :date, :on => :create, :on_or_after => :today
+  validates :date, presence: true
+  validates :date, uniqueness: true
 end
