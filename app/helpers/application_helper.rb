df93645ac0c3a508d90cdde6895170f6456b455f
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:50:55 +0000

    Added current_user, logged_in?, login_required, & already_logged_in? methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_required
+    redirect_to login_path if !logged_in?
+  end
+
+  def already_logged_in?
+    redirect_to clients_path if logged_in?
+  end
 end
