f756309f6543f63e0f222941c6e58c5513f0b1ae
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:25:05 +0000

    Completed new & create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def new
-    
+    @user = User.new
   end
 
   def create
-  
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to clients_path
+    else
+      render :new
+    end
   end
 
   private
