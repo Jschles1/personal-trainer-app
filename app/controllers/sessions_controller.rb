59fbe033597ec10686a271e9c41cb22a5e06f561
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:28:36 +0000

    Completed new, destroy, & most of create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
-
+    @user = User.new
+    @error = nil
   end
 
   def create
-
+    if !params[:provider].nil?
+      oauth_login
+    elsif params[:user][:email].blank? || params[:user][:password].blank?
+      # redirect to login form with errors
+    else
+      normal_login
+    end
   end
 
   def destroy
-
+    session.clear
+    redirect_to login_path
   end
 
 end
