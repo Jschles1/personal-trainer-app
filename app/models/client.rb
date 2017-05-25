b5f0baabc708417e2c63bffc030d17accd229112
John Schlesinger <jschles1@ramapo.edu>
2017-05-25 02:57:19 +0000

    Added client model validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,10 @@ class Client < ApplicationRecord
   has_many :appointments
   has_many :gyms, through: :appointments
   belongs_to :user
+
+  validates :name, :email, :phone, :age, :weight, :goal, presence: true
+  validates :email, uniqueness: true, email: true
+  validates :age, numericality: { greater_than: 0, less_than: 100 }
+  validates :weight, numericality: { greater_than: 0 }
+  validates :phone, phone: true
 end
