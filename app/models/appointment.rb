6d558e9310ddb88f06decdd72036aa28a051b581
John Schlesinger <jschles1@ramapo.edu>
2017-05-30 03:15:27 +0000

    Added client_attributes= method

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,4 +6,10 @@ class Appointment < ApplicationRecord
   validates_datetime :date, :on => :create, :on_or_after => :today
   validates :date, presence: true
   validates :date, uniqueness: true
+
+  def client_attributes=(client_attributes)
+    if client_attributes[:name] != ""
+      self.client = Client.find_or_create_by(client_attributes)
+    end
+  end
 end
