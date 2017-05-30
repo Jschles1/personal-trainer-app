6cebd3a4b695ba51f2d9bee0e03848a1963b1661
John Schlesinger <jschles1@ramapo.edu>
2017-05-30 03:29:12 +0000

    Removed belongs_to rails 5 requirement, added routes for appointments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :clients
+  resources :appointments
   resources :sessions, only: [:create]
 
   get '/signup', to: 'users#new', as: 'signup'
