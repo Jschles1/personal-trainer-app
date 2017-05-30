f0453e4ca4643c0e6a5b4e74f9b6344dcf1c7e82
John Schlesinger <jschles1@ramapo.edu>
2017-05-30 03:08:42 +0000

    Completed new, index, create actions

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,21 @@
 class AppointmentsController < ApplicationController
 
   def index
+    @appointments = current_user.appointments
   end
 
   def new
+    @appointment = current_user.appointments.build
+    @appointment.build_client
   end
 
   def create
+    @appointment =  Appointment.new(appointment_params.merge(user_id: current_user.id))
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render :new
+    end
   end
 
   def edit
